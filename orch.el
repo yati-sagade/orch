202c67ee1a24ad991d9508eeaeb0deec0c1c57da
Yati Sagade <yati.sagade@gmail.com>
2016-04-23 16:13:32 +0200

    Add license

--- orch.el
+++ orch.el
@@ -1,5 +1,24 @@
-;; orch.el
+;; orch.el --- Emacs server for importing scribbles from the orch android app
+
+;; Version: 0.1.0
+;; Author: Yati Sagade <yati.sagade@gmail.com>
+;; Package-Requires: ((web-server "0.1.1"))
+;; Url: https://github.com/yati-sagade/orch
+
+;; This software is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This software is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
 ;; 
+
 ;;
 ;; We start a very simple HTTP server (by default on port 4000), which has two endpoints:
 ;;
